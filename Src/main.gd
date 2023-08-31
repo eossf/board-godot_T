f61d73e8a94f9b7e5ed446fc90fb87574f9df57e
metairie <stephane.metairie@gmail.com>
2023-08-31 13:42:07 +0200

    add infos

--- Src/main.gd
+++ Src/main.gd
@@ -1,29 +1,21 @@
 extends Node2D
+@onready var grid = %Grid
 
 @onready var background = %background
-@onready var timer_background = %timer_background
 @onready var screen_width : float = ProjectSettings.get_setting("display/window/size/viewport_width")
 @onready var screen_height : float = ProjectSettings.get_setting("display/window/size/viewport_height")
 @onready var screen_size : Vector2 = Vector2(screen_width, screen_height)
 @onready var screen_center : Vector2 = screen_size / 2.0
-var step_scale : Vector2 = Vector2(0.0005, 0.0005)
-var target_scale : Vector2 = Vector2(1.05,1.05)
+
+@export var zoom_duration := 1
+@export var color_duration := 0.5
+@export var zoom_level := 1.1
+
+var char_tex = load("res://Images/720x1008_pixel_board_game-v0.0.0-001-final.png") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_button_pressed():
-	pass
-
-func _on_timer_background_timeout():
-	print("ok " + str(background.scale) + " target " + str(target_scale))
-	if background.scale < target_scale:
-		# zoom on background image
-		background.scale = background.scale + step_scale
-	else:
-		timer_background.stop()
+	var tween = get_tree().create_tween()
+	tween.tween_property(%background, "scale", Vector2(zoom_level, zoom_level), zoom_duration).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(%background, "modulate", Color.LIGHT_GREEN, color_duration)
+	background.texture = char_tex
